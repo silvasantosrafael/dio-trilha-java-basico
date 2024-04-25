92c5a4afb01e881f0499ad46e999900fda02ef83
silvasantosrafael <rafael.silva318819@gmail.com>
2024-04-24 23:04:30 -0300

    Development completed

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        int numero;
+        String agencia;
+        String nomeCliente;
+        BigDecimal saldo;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Por favor, digite o número da Agência: ");
+        agencia = input.nextLine();
+        System.out.print("Por favor, digite o número da Conta: ");
+        numero = input.nextInt();
+        input.nextLine();
+        System.out.print("Por favor, digite o seu Nome: ");
+        nomeCliente = input.nextLine();
+        System.out.print("Por favor, digite o Saldo: ");
+        saldo = input.nextBigDecimal();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
     }
 }
